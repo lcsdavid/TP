8476f9caecc7b3d1fa76fcf4aa3c9d2a1ab06e67
ThLegras <legrastheo@gmail.com>
2019-04-05 17:39:14 +0200

    ça devrait marcher

--- src/fr/lcsdavid/rmi/client/Client.java
+++ src/fr/lcsdavid/rmi/client/Client.java
@@ -1,5 +1,6 @@
 package fr.lcsdavid.rmi.client;
 
+import fr.lcsdavid.rmi.Article;
 import fr.lcsdavid.rmi.ArticleNotFound;
 import fr.lcsdavid.rmi.Catalogue;
 import fr.lcsdavid.rmi.IManager;
@@ -10,44 +11,55 @@ import java.util.Scanner;
 
 public class Client {
 
-    private static String Choix() {
+    private static String Choix(Scanner sc) {
         System.out.println("Pour quitter entrez -1");
         System.out.println("Pour afficher tous les produits entrez 0");
         System.out.println("Pour afficher un produit entrez sa clé");
         System.out.println("Pour interagire avec le panier entrez 1");
-        Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         return str;
     }
 
-    private static String ChoixPanier() {
+    private static String ChoixPanier(Scanner sc) {
 
         System.out.println("Pour ne rien faire et retourner à l'affichage général entrez -1");
         System.out.println("Pour afficher tous les produits du panier entrez 0");
         System.out.println("Pour ajouter ou retirer un ou plusieurs produits entrez sa clé");
         System.out.println("Pour afficher le prix du panier entrez 1");
-        Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         return str;
     }
 
-    private static void manipulation(String str, Catalogue catalogue) {
+    private static void manipulation(String str, Catalogue catalogue, Panier panier, Scanner sc) {
 
         if (str.equals("0"))
             System.out.println(catalogue.toString());
         else if (str.equals("1")) {
-            str = ChoixPanier();
+            str = ChoixPanier(sc);
             if (str.equals("-1"))
                 return;
             else if (str.equals("0"))
-                //TODO;
-                ;
+                System.out.println(panier.toString());
             else if (str.equals("1"))
-                //TODO
-                ;
-            else
-                //TODO
-                ;
+                System.out.println(panier.calculMontantPanier());
+            else{
+                String produit = str;
+                System.out.println("Entrez la quantité de produit désirée");
+                int qt;
+                qt = sc.nextInt();
+                try {
+                    panier.modifierQuantitéArticle(catalogue.article(produit), qt);
+                } catch (ArticleNotFound articleNotFound) {
+                    try {
+                        panier.ajouterArticle(catalogue.article(produit));
+                        panier.modifierQuantitéArticle(catalogue.article(produit), qt);
+                    } catch (ArticleNotFound articleNotFound1) {
+                        articleNotFound1.printStackTrace();
+                    }
+                }
+
+
+            }
         }
         //on est au moment où le client à forcement entré une clé (ou aurait du)
         else if (catalogue.isArticle(str))//si la clé existe
@@ -62,6 +74,7 @@ public class Client {
     }
 
     public static void main(String[] args) throws Exception {
+
         /* On récupère le Registry disponible sur la machine 'localhost' sur le port 1099. */
         Registry registry = LocateRegistry.getRegistry("localhost", 1099);
         /* On crée un objet Hello qui correspond à l'object distant stockée en tant que 'myhello'. */
@@ -72,15 +85,16 @@ public class Client {
         IManager manager = (IManager) registry.lookup("manager");
         Catalogue catalogue = manager.catalogue();
         //System.out.println(catalogue.toString());
-
+        Panier panier = new Panier();
         Boolean shopping = true;
 
+        Scanner sc = new Scanner(System.in);
         while (shopping) {
-            String res = Choix();
+            String res = Choix(sc);
 
             if (res.equals("-1"))
                 break;
-            manipulation(res, catalogue);
+            manipulation(res, catalogue, panier, sc);
         }
     }
 }
